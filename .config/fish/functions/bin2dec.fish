76077098a558368cb3697a2e00e49fa3a731a161
schulle <cschult@devmem.de>
2019-04-25 07:33:30 +0200

    bin2dec + bin2hex: add option -h --help

--- .config/fish/functions/bin2dec.fish
+++ .config/fish/functions/bin2dec.fish
@@ -6,12 +6,22 @@ function bin2dec
         echo "example: $_ 1010"
         echo "output: 10"
     end
-    if echo $argv | egrep '^[0,1]+$' >/dev/null 2>&1
+
+    set -l options (fish_opt --short=h --long=help)
+    argparse --name=bin2dec $options -- $argv
+    or return
+    if set -q _flag_h; or set -q _flag_help
+        _help
+        return
+    end
+
+    if test -z $argv
+        return
+    else if echo $argv | egrep '^[0,1]+$' >/dev/null 2>&1
         echo "obase=10;ibase=2;$argv" | bc
     else
         echo "not a binary number"
         echo
-        _help
     end
 end
 
--- .config/fish/functions/bin2hex.fish
+++ .config/fish/functions/bin2hex.fish
@@ -6,7 +6,18 @@ function bin2hex
         echo "example: $_ 1010"
         echo "output: 0xA"
     end
-    if echo $argv | egrep '^[0,1]+$' >/dev/null 2>&1
+
+    set -l options (fish_opt --short=h --long=help)
+    argparse --name=bin2dec $options -- $argv
+    or return
+    if set -q _flag_h; or set -q _flag_help
+        _help
+        return
+    end
+
+    if test -z $argv
+        return
+    else if echo $argv | egrep '^[0,1]+$' >/dev/null 2>&1
         set hex (echo "obase=16;ibase=2;$argv" | bc)
         echo "0x$hex"
     else
