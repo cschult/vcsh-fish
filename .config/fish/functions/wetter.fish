a4d5988be949af054b75fcce9644aa7f00f0b138
schulle <cschult@devmem.de>
2019-04-25 22:52:03 +0200

    wetter: make Hamburg default if no location given

--- .config/fish/functions/wetter.fish
+++ .config/fish/functions/wetter.fish
@@ -1,4 +1,14 @@
-# Defined in - @ line 1
 function wetter --description 'alias wetter curl http://wttr.in/Hamburg'
-	curl http://wttr.in/Hamburg $argv;
+
+    #set -l options (fish_opt --short=l --long=lang)
+    #argparse --name=wetter $options -- $argv
+    #or return
+
+    if test -z $argv
+        set argv 'Hamburg'
+    end
+
+    # curl "wttr.in/Paris?lang=fr
+    command curl http://de.wttr.in/$argv
 end
+
