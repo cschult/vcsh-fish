9a92eaa3fc668afc4bb4152e2b5b5caadde9670e
schulle <cschult@devmem.de>
2019-04-25 07:38:34 +0200

    hex2dec: add option -h --help

--- .config/fish/functions/hex2dec.fish
+++ .config/fish/functions/hex2dec.fish
@@ -1,5 +1,26 @@
 function hex2dec
-    if echo $argv | egrep '^[[:alnum:]]+$' >/dev/null 2>&1
+    function _help
+        echo "usage:"
+        echo "$_ <hex number>"
+        echo
+        echo "example: $_ F"
+        echo "output: 15"
+        echo
+        echo "example $_ 0xF"
+        echo "output: 15"
+    end
+
+    set -l options (fish_opt --short=h --long=help)
+    argparse --name=bin2dec $options -- $argv
+    or return
+    if set -q _flag_h; or set -q _flag_help
+        _help
+        return
+    end
+
+    if test -z $argv
+        return
+    else if echo $argv | egrep '^[[:alnum:]]+$' >/dev/null 2>&1
         set x (string replace -r '^0x' '' $argv)
         set x (string upper $x)
         echo "obase=10;ibase=16;$x" | bc
