37e82223ecb73dcfaab124942f70030b0647a559
schulle <cschult@devmem.de>
2019-04-27 11:26:35 +0200

    function wetter: add default place 'Hamburg', call wttr.in

--- .config/fish/functions/wetter.fish
+++ .config/fish/functions/wetter.fish
@@ -45,29 +45,37 @@ function wetter --description 'alias wetter curl http://wttr.in/Hamburg'
 
     while true
         if set -q _flag_p
-            echo \$_flag_p: $_flag_p
+            # echo \$_flag_p: $_flag_p
             if string match -q -r '^-' -- $_flag_p
                 echo "not a valid parameter for option -p/--place: $_flag_p"
                 return 3
+            else
+                set _args $_flag_p
             end
+        else
+            set _args 'Hamburg'
         end
         if set -q _flag_l
-            echo \$_flag_l: $_flag_l
+            # echo \$_flag_l: $_flag_l
             if string match -q -r '^-' -- $_flag_l
                 echo "not a valid parameter for option -l/--lang: $_flag_l"
                 return 3
+            else
+                set _args (string join '' $_args "?lang=" $_flag_l)
             end
+        else
+            set _args (string join '' $_args "?lang=" "de")
         end
         break
     end
 
+    # echo $_args
     if not test -z $argv
         echo "non option parameter found: $argv"
         return 4
     end
 
-    # curl "wttr.in/Paris?lang=fr
-    echo "command curl http://wttr.in/$optargs"
-    #command curl http://wttr.in/$_param
+    # echo "command curl http://wttr.in/$_args"
+    command curl http://wttr.in/$_args
 end
 
