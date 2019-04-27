142a0ff61f2c33c026f95faa4d67f066db5f5ebd
schulle <cschult@devmem.de>
2019-04-27 11:30:14 +0200

    function wetter: remove old commented out code, change description

--- .config/fish/functions/wetter.fish
+++ .config/fish/functions/wetter.fish
@@ -1,44 +1,6 @@
-function wetter --description 'alias wetter curl http://wttr.in/Hamburg'
+function wetter --description 'get weather forecast from wttr.in/'
 
     set -l _name (status current-command)
-    #set -e _place
-    #set -e _lang
-
-    ##argparse --name=$_name 'p/place=' -- $argv
-    #argparse --name=$_name 'p/place=' 'l/lang=' -- $argv
-    #or return
-
-    #if set -q _flag_p
-    #    echo \$_flag_p: $_flag_p
-    #    set _param $_flag_p
-    #else
-    #    set _param "Hamburg"
-    #end
-    #if set -q _flag_l
-    #    echo \$_flag_l: $_flag_l
-    #    # set lang '?lang='
-    #    set _param (string join '' $_param "?lang=$_flag_l")
-    #end
-
-    #while set optarg (getopts "l:lang: p:place:" $argv)
-    #    echo $__getopts_optstr
-    #    switch $optarg[1]
-    #        case l
-    #            echo echo $optarg[1] $optarg[2]
-    #        case p
-    #            echo echo $optarg[1] $optarg[2]
-    #        case \*
-    #            echo unknown option `$optarg[1]`
-    #    end
-    #end
-    #echo operands: "$optarg"
-
-    #while set optarg (getopt -n $_name -o 'p:l:' -l 'place:,lang:' -- $argv)
-    #    switch $optarg[1]
-    #        case -p --place
-    #            echo $optarg[1]: $optarg[2]
-    #    end
-    #end
 
     argparse --name=$_name 'p/place=' 'l/lang=' -- $argv
     or return
