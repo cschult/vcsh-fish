58c73d7d0c99614b22c2989c3c635dc1c3e0a560
schulle <cschult@devmem.de>
2019-04-29 00:38:36 +0200

    nicer formatting

--- .config/fish/functions/tart.fish
+++ .config/fish/functions/tart.fish
@@ -1,38 +1,28 @@
 function tart
 
     set _my_name (status current-command)
+
     function usage
         echo "list content of various compreѕsed files"
         echo "usage: $_my_name <compressedfile>"
     end
-    if test -z $argv[1]
+
+    if test -z $argv[1] || test $argv[1] = '-h'
         usage
         return
     end
-    if test $argv[1] = '-h'
-        usage
-        return
-    end
+
     if test -f $argv[1]
         switch $argv[1]
-            case '*.tar.bz2' '*.tbz'
-                tar tjf $argv[1]
-            case '*.tar.gz' '*.tgz'
-                tar tzf $argv[1]
-            case '*.tar.xz'
-                tar tJf $argv[1]
-            case '*.tar'
-                tar tf $argv[1]
-            case '*.zip'
-                unzip -l $argv[1]
-            case '*.rar'
-                unrar l $argv[1]
-            case '*.gz' '*.gzip'
-                gunzip -l $argv[1]
-            case '*.Z'
-                uncompress -l $argv[1]
-            case '*'
-                echo "unknown archiv format"
+            case '*.tar.bz2' '*.tbz';       tar tjf $argv[1]
+            case '*.tar.gz' '*.tgz';        tar tzf $argv[1]
+            case '*.tar.xz';                tar tJf $argv[1]
+            case '*.tar';                    tar tf $argv[1]
+            case '*.zip';                  unzip -l $argv[1]
+            case '*.rar';                   unrar l $argv[1]
+            case '*.gz' '*.gzip';         gunzip -l $argv[1]
+            case '*.Z';               uncompress -l $argv[1]
+            case '*' echo "unknown archiv format"
         end
     else
         echo "not a valid file"
