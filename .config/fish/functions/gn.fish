5c1327c62db1070ee8f8fec032a270924c7ddda9
schulle <cschult@devmem.de>
2019-04-28 11:34:46 +0200

    function gn: test if we are in a git repo

--- .config/fish/functions/gn.fish
+++ .config/fish/functions/gn.fish
@@ -3,6 +3,12 @@ function gn --description 'show name of current git repository'
     # show name of current git repo. When in vcsh, the
     # remotes name is used
 
+    git status > /dev/null 2>&1
+    if test $status -eq 128
+        echo not a git repository
+        return
+    end
+
     if test (git rev-parse --show-toplevel) = $HOME
         basename (git remote get-url origin)
     else
