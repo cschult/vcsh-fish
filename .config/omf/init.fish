9649b4ed28191b75fcffec0dd4b5f9e823e94f4d
schulle <cschult@devmem.de>
2019-05-04 14:59:10 +0200

    minor change

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,18 +1,18 @@
-# begin: theme bobthefish
+# theme bobthefish
 # set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 # set -g theme_display_date no
 # set -g theme_display_git_master_branch yes
 # set -g theme_nerd_fonts yes
 # set -g theme_newline_cursor yes
-#set -g theme_newline_prompt '$ '
+# set -g theme_newline_prompt '$ '
 # end: theme bobthefish
-# begin: theme kawasaki
-#### Disable playing the user's current group.
+# theme kawasaki
+#### Disable displaying the user's current group.
 set -g theme_display_group no
 #### Hide the current directory read/write indicator.
 set -g theme_display_rw no
 #### Don't display the VirtualEnv prompt.
 set -g theme_display_virtualenv no
-
+# end: theme kawasaki
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
