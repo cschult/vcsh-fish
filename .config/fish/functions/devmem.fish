f0752d9ebcf1e006f720c53d936943a356aac39e
schulle <schulle@jpop>
2019-05-14 06:52:21 +0200

    nicer comment

--- .config/fish/functions/devmem.fish
+++ .config/fish/functions/devmem.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function devmem --description 'alias h16=ssh -t devmem.de "if tmux has -t devmem; then tmux attach -t devmem; else tmux new -s devmem; fi"'
-	ssh -t devmem.de "if tmux has -t devmem; then tmux attach -t devmem; else tmux new -s devmem; fi" $argv;
+function devmem --description "ssh to devmem.de and start or enter tmux session called 'devmem'"
+    ssh -t devmem.de "if tmux has -t devmem; then tmux attach -t devmem; else tmux new -s devmem; fi"
 end
