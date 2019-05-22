3d9ed520c7a834a8413eb8096d1c173b87ca0cc7
schulle <cschult@devmem.de>
2019-05-22 20:36:36 +0200

    fix function devmem

--- .config/fish/functions/devmem.fish
+++ .config/fish/functions/devmem.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function devmem --description "ssh to devmem.de and start or enter tmux session called 'devmem'"
-    ssh -t devmem.de "if tmux has -t devmem; then tmux attach -t devmem; else tmux new -s devmem; fi"
+    ssh -t devmem.de "sh -c 'if tmux has -t devmem; then tmux attach -t devmem; else tmux new -s devmem; fi'"
 end
