c481d4b2dbbddb76d6b974ae6ec34f4832ddfe14
schulle <cschult@devmem.de>
2019-09-05 11:35:37 +0200

    minor change

--- .config/fish/functions/mt.fish
+++ .config/fish/functions/mt.fish
@@ -1,4 +1,8 @@
 function mt --description 'view monit status with elinks'
+    # (C) cschult@devmem.de 05.09.2019
+    # Ssh tunnel is established at boot as a systemd user service
+    # (~/.config/systemd/user/monit-tunnel.service), which uses
+    # autossh.
     elinks https://schulle@localhost:2812
 end
 
